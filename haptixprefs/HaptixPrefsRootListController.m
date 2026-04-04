cd711cc49abea64ec19f323c5021a5b451cd6e5d
Eoln Msuk <90252497+EolnMsuk@users.noreply.github.com>
2026-04-04 01:56:07 -0700

    Update HaptixPrefsRootListController.m

--- haptixprefs/HaptixPrefsRootListController.m
+++ haptixprefs/HaptixPrefsRootListController.m
@@ -10,4 +10,28 @@
     return _specifiers;
 }
 
+- (void)resetSettings {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Reset HaptiX" 
+                                                                   message:@"Are you sure you want to reset all HaptiX settings to their defaults?" 
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+
+    UIAlertAction *confirm = [UIAlertAction actionWithTitle:@"Reset" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * action) {
+        // Delete preference file
+        [[NSFileManager defaultManager] removeItemAtPath:@"/var/jb/var/mobile/Library/Preferences/com.eolnmsuk.haptix.plist" error:nil];
+        
+        // Notify tweak to reload defaults
+        CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), CFSTR("com.eolnmsuk.haptix/ReloadPrefs"), NULL, NULL, YES);
+        
+        // Visually reload the preference pane
+        [self reloadSpecifiers];
+    }];
+
+    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
+
+    [alert addAction:confirm];
+    [alert addAction:cancel];
+
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
 @end
